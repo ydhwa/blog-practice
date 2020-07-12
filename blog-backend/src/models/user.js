9a1d3c140609c4f9b4d36fe65803bb6abcd22681
YDH <ydhwa_18@naver.com>
2020-07-12 22:04:51 +0900

    [blog-backend] Add schema/model of user

---
+++ blog-backend/src/models/user.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; // true | false
+};
+
+// 스태틱 메서드
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
