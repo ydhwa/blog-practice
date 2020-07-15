6c08a3b0c5d1906749fe3fb2877287b39bd31f9b
YDH <ydhwa_18@naver.com>
2020-07-16 00:40:10 +0900

    [blog-frontend] Add header component, save login status in localStorage

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -74,9 +74,12 @@ const RegisterForm = ({ history }) => {
   // user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
-      console.log('check API 성공');
-      console.log(user);
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify);
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [history, user]);
 
